58688c1d15b1d3648342f90a00087ab959d266ac
Merchat <alexandre.merchat@outlook.fr>
2023-09-27 10:27:46 +0200

    first

---
+++ Data/film.py
@@ -0,0 +1,7 @@
+pictures = [
+    {"id": 0, "title": "Inception", "director": "Christopher Nolan"},
+    {"id": 1, "title": "The Shawshank Redemption", "director": "Frank Darabont"},
+    {"id": 2, "title": "Pulp Fiction", "director": "Quentin Tarantino"},
+    {"id": 3, "title": "The Godfather", "director": "Francis Ford Coppola"},
+    {"id": 4, "title": "The Dark Knight", "director": "Christopher Nolan"}
+]
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask , render_template
+import random
+from Data.film import pictures
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+       return render_template('home.html')
+
+@app.route('/pictures')
+def show_pictures():
+       return render_template('pictures.html', pictures=pictures)
+
+@app.route('/api/picture/<id>')
+
+def getpictureid(id):
+       id=int(id)
+       picture=None
+       if 0 <= id < len(pictures):
+        picture = pictures[id]
+       return render_template('picture.html', picture = picture)
+
+# def shuffle_pictures():
+#        random.shuffle(pictures)
+
+# if __name__ == '__main__':
+#        shuffle_pictures()
+#        app.run(debug=True)
